7932083accb8002df1310ee7bee23babe59a12f3
RANAJOY NAG <iemrono2022@gmail.com>
2025-07-17 15:51:25 +0000

    change the font to another cool font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
