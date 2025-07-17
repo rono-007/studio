efe75d194a170dfc1a8b8df4d275a45035fe2a67
RANAJOY NAG <iemrono2022@gmail.com>
2025-07-17 10:51:15 +0000

    roll back to previous state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -237,7 +237,7 @@ export default function Home() {
           </Button>
         </SidebarHeader>
         <SidebarContent>
-          <SidebarMenu className="mt-4">
+          <SidebarMenu>
             {sessions.map((session) => (
               <SidebarMenuItem key={session.id}>
                 {editingSessionId === session.id ? (
